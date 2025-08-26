48266b73c7d77b39b46b0a0ebbd41661db888539
shrutijadhavKH <shruti.jadhav.cmaug25@gmail.com>
2025-08-26 09:02:29 +0530

    Add files via upload
    Assignment_02

---
+++ DataType.java
@@ -0,0 +1,26 @@
+public class DataTypesCheck {
+    
+    // Instance variables 
+    byte a;       
+    short b;     
+    int c;        
+    long d;    
+    float e;     
+    double f;    
+    char g;       
+    boolean h;    
+
+    public static void main(String[] args) {
+        DataTypesCheck obj = new DataTypesCheck();
+
+     
+        System.out.println("byte a: " + obj.a);
+        System.out.println("short b: " + obj.b);
+        System.out.println("int c: " + obj.c);
+        System.out.println("long d: " + obj.d);
+        System.out.println("float e: " + obj.e);
+        System.out.println("double f: " + obj.f);
+        System.out.println("char g: '" + obj.g + "'");
+        System.out.println("boolean h: " + obj.h);
+    }
+}
